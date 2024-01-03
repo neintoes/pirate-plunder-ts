b871426c41af62703fee5c49c2d92b6659edc7df
Ashley <ashley@thepaynes.uk>
2024-01-03 15:48:12 +0000

    Fix build error

--- Treasure.ts
+++ Treasure.ts
@@ -1,44 +1,28 @@
-// GH1
 interface iCollectable {
     value: number;
 }
-
-class Treasure extends sprites.ExtendableSprite implements iCollectable{
+class Treasure extends sprites.ExtendableSprite implements iCollectable {
     public value: number;
-    private hitbox: Sprite;
-
+    public hitbox: Sprite;
     constructor(spawnPoint: tiles.Location) {
         super(assets.image`treasure`, SpriteKind.Treasure);
         tiles.placeOnTile(this, spawnPoint);
-        new Hitbox(this);
         this.value = randint(500, 2000);
+        this.hitbox = new Hitbox(this, 20, SpriteKind.TreasureHitbox);
     }
 }
-
-class Hitbox extends sprites.ExtendableSprite{
-    public parent: Treasure;
-
-    constructor(parent: Treasure) {
-        super(image.create(47, 47), SpriteKind.Hitbox)
-        this.parent = parent;
-        spriteutils.drawCircle(this.image, 24, 24, 20, 1)
-        this.setFlag(SpriteFlag.Invisible, true)
-        tiles.placeOnTile(this, this.parent.tilemapLocation());
-    }
-}
-
 class TreasureSpawner {
-    constructor() {
+    playerSprite: PlayerSprite;
+    constructor(playerSprite: PlayerSprite) {
+        this.playerSprite = playerSprite;
         for (let i = 0; i < 10; i++) {
             this.treasureSpawning();
         }
     }
-
     public treasureSpawning(): void {
-        if(sprites.allOfKind(SpriteKind.Treasure).length < 10) {
+        if (sprites.allOfKind(SpriteKind.Treasure).length < 10) {
             let tile = tilesAdvanced.getAllWallTiles()._pickRandom();
             new Treasure(tile);
         }
     }
 }
-// end GH1
